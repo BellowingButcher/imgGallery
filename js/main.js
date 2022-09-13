e51ba4bc418935af1365bac59c7712990ff25060
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-13 22:25:16 +0000

    adds const newImage this creates an img element sets src to xxx sets alt to xxx then appends to const thumbBar, which is using the querySelector to find any elements with the css class of .thumbBar

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const imgFolder = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /*declaring the array of alt text for each image in img folder*/
 
 const altText = ['Blue human eye', 'Aerial photo of sand dunes', 'Purple flowers', 'Egyptian hieroglyphics', 'Orange butterfly on a green leaf'];
+
 /*
 Loop through the array of imgFolder
     for each pic
@@ -17,6 +18,11 @@ Loop through the array of imgFolder
         (the documentation says this should embed that picture along with its alt text into the
         page)
 */
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
 
 /* 
 Add click event listener to each img inside the thumb-bar div
@@ -29,10 +35,11 @@ Add click event listener to the <button>
 When clicked the darken effect is applied to the full-size image.
 When clicked again the darken effect is removed.
 */
-
+/* THIS IS COMMENTED SO I CAN DEBUGG EARLIER STUFF
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
-
+*******
+*/
 /* Wiring up the Darken/Lighten button */
