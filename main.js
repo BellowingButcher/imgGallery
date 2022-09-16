5724e379722534f5538a807729fb22199f8b7b31
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-16 08:28:59 +0000

    half way through the replace

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const picName = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
+for (image of picName) {
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
-
+newImage.src.replace('xxx', '')
+}
 /* Wiring up the Darken/Lighten button */
