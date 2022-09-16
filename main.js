26d0242eb743e7e5d6922f9cf51ba4a2da0dddc4
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-16 08:44:52 +0000

    This has the image gallery with the photos correctly pathed

--- main.js
+++ main.js
@@ -8,14 +8,19 @@ const overlay = document.querySelector('.overlay');
 const picName = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
-
+const picAlt = ['A human eye', 'An aerial photo of sand dunes', 'purple flowers', 'Ejyption Hyroglyphs', 'Butterfly on a green leaf']
 /* Looping through images */
 for (image of picName) {
 
+    let newSrc = image;
+    let newAlt = image;
+
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', 'images/' + newSrc);
+newImage.setAttribute('alt', newAlt);
 thumbBar.appendChild(newImage);
-newImage.src.replace('xxx', '')
+
 }
 /* Wiring up the Darken/Lighten button */
+
+images/pic1.jpg
